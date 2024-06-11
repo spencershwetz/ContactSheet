c1ce4a47212cd1cb9c9ee8c20d4c72450357888b
windy <windywu812@gmail.com>
2024-06-11 21:39:28 +0700

    Convert to UIKit project

---
+++ Contact Sheet/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Contact Sheet
+//
+//  Created by Windy on 11/06/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+}
---
+++ Contact Sheet/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Contact Sheet
+//
+//  Created by Windy on 11/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
